5eb9f6f7146988ae4b6551b3b58fe5f0e3938946
Rahul Sharma <rahulsharma@Rahuls-MacBook-Air.local>
2023-11-16 12:27:33 +0530

    Ticket Availability Blank commit

---
+++ src/main/java/com/ticketAvailability/TicketAvailabilityApplication.java
@@ -0,0 +1,13 @@
+package com.ticketAvailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAvailabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAvailabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketAvailability/TicketAvailabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketAvailability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketAvailabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
